1cab25a8de7994d5a4304aa03333a7a69abd3391
Andriy <olshanskyy49@gmail.com>
2017-12-10 16:48:42 +0200

    devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
-  def new
-  end
+     def new
+        @user=User.new
+     end
+
+     def show
+     end
+
+     def create
+     end
+
+     def destroy
+     end
+
+     def edit
+     end
 end
